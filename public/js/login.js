6c88174fbecbc02a2fa716d710724fe84584d16a
Saloni0412 <pshashikantb@gmail.com>
2023-06-16 16:54:54 -0400

    this commit adds css class in logout.js/login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,23 +1,22 @@
 const login = async (event) => {
-    event.preventDefault();
-  
-    const name = document.querySelector('#classname').value.trim();
-    const password = document.querySelector('#classname').value.trim();
-  
-    if (name && password) {
-      const response = await fetch('/api/whereverlogincodeis', {
-        method: 'POST',
-        body: JSON.stringify({ name, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/whatevertheloginpageis');
-      } else {
-        alert(response.statusText);
-      }
+  event.preventDefault();
+
+  const username = document.querySelector('.login-user').value.trim();
+  const password = document.querySelector('.login-pass').value.trim();
+
+  if (username && password) {
+    const response = await fetch('/api/user', {
+      method: 'POST',
+      body: JSON.stringify({ name, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/book');
+    } else {
+      alert(response.statusText);
     }
-  };
-  
-  document.querySelector('class/idname').addEventListener('submit', login);
-  
+  }
+};
+
+document.getElementById('login-btn').addEventListener('submit', login);
