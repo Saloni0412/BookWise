e41dbd43bf8ceb54eef0762cfcafd7bd0b6ba31f
Saloni0412 <pshashikantb@gmail.com>
2023-06-17 18:12:08 -0400

    this commit adds form and added css properties to event handlers

--- public/js/addingBook.js
+++ public/js/addingBook.js
@@ -29,29 +29,29 @@ const newFormHandler = async (event) => {
   }
 };
 
-const delButtonHandler = async (event) => {
-  //need to change class or id according to handlebars
-  if (event.target.hasAttribute("")) {
-    const id = event.target.getAttribute("");
+// const delButtonHandler = async (event) => {
+//   //need to change class or id according to handlebars
+//   if (event.target.hasAttribute("")) {
+//     const id = event.target.getAttribute("");
 
-    const response = await fetch(`/api/book/${id}`, {
-      method: "DELETE",
-    });
+//     const response = await fetch(`/api/book/${id}`, {
+//       method: "DELETE",
+//     });
 
-   //need to change class or id according to handlebars
-    if (response.ok) {
-      document.location.replace("");
-    } else {
-      alert("Failed to delete project");
-    }
-  }
-};
+//    //need to change class or id according to handlebars
+//     if (response.ok) {
+//       document.location.replace("");
+//     } else {
+//       alert("Failed to delete project");
+//     }
+//   }
+// };
 
 //need to change class or id according to handlebars
 document
-  .querySelector('')
+  .querySelector('.submit-btn')
   .addEventListener('submit', newFormHandler);
 
-document
-  .querySelector('')
-  .addEventListener('click', delButtonHandler);
+// document
+//   .querySelector('')
+//   .addEventListener('click', delButtonHandler);
