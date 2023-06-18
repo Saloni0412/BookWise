4a9445c236c8bc7d1c83003a162d7deb6fdebcd1
Andre Besner <besner.andre@gmail.com>
2023-06-18 10:48:19 -0400

    add genre input to add book form. change aria labels for screen read compat

--- models/index.js
+++ models/index.js
@@ -2,12 +2,12 @@ const User = require('./User');
 const Book = require('./Book');
 
 User.hasMany(Book, {
-    foreinKey: 'user_id',
+    foreignKey: 'user_id',
     onDelete: 'CASCADE'
 });
 
 Book.belongsTo(User, {
-    foreinKey: 'user_id'
+    foreignKey: 'user_id'
 });
 
 module.exports = { User, Book }
--- public/js/addingBook.js
+++ public/js/addingBook.js
@@ -1,18 +1,19 @@
+
 // import image url function
 // const getBookImage = require('../../public/js/bookImage');
 
 const newFormHandler = async (event) => {
   event.preventDefault();
 
-  //need to change class or id according to handlebars
   const name = document.querySelector("#new-book-name").value.trim();
   const author = document.querySelector("#new-book-author").value.trim();
+  const genre = document.querySelector("#new-book-genre").value.trim();
 
   if (name && author) {
     // const imageURL = await getBookImage(name);
     const response = await fetch(`/api/book`, {
       method: "POST", 
-      body: JSON.stringify({ name, author /*, imageURL*/ }),
+      body: JSON.stringify({ name, author, genre /*, imageURL*/ }),
       headers: {
         "Content-Type": "application/json",
       },
